a9eeb9785e2724510dced2a88849ed35938af39b
Pritam Mandal <pritampmandal@gmail.com>
2025-07-03 19:42:12 +0530

    removed the changes i.e, secure_url from cloudinary.js

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,10 +1,10 @@
 import { v2 as cloudinary } from "cloudinary"
 import fs from "fs"
- 
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET 
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -22,7 +22,7 @@ const uploadOnCloudinary = async (localFilePath) => {
         //file has been uploaded successfully
         // console.log("file is uploaded on Cloudinary", response.url)
         fs.unlinkSync(localFilePath)
-        return response.secure_url;
+        return response;   // Return the HTTPS version of the uploaded file URL to avoid mixed content issues on frontend
 
     } catch (error) {
         //agar hamari file uplaod nahi hoti hain , to usse local server se bhe toh hatana pdega na...
@@ -32,4 +32,4 @@ const uploadOnCloudinary = async (localFilePath) => {
 }
 
 
-export {uploadOnCloudinary}
+export { uploadOnCloudinary }
