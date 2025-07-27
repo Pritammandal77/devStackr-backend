3168802a870af721426c71c9a0c50f3647a747f3
Pritam Mandal <pritampmandal@gmail.com>
2025-07-27 21:00:03 +0530

    setup fully working socket.io

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 import connectDB from "./db/index.js"
 import dotenv from "dotenv"
-import { app } from "./app.js"
+import { app, server } from "./app.js"
 
 //As early as possible in your application, import and configure dotenv
 dotenv.config({
@@ -10,7 +10,7 @@ dotenv.config({
 
 connectDB()
 .then(
-    app.listen(process.env.PORT || 8000, () => {
+    server.listen(process.env.PORT || 8000, () => {
         console.log(`Server is running at port : ${process.env.PORT}`)
     })
 )
